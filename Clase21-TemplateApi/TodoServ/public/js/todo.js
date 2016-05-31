917c5c8fa4ece3dd59d48dd7893700889b95ee89
Julian <julianreinoso94@gmail.com>
2016-05-31 16:49:34 -0300

    agregar template a api Persona

---
+++ Clase21-TemplateApi/TodoServ/public/js/todo.js
@@ -0,0 +1,46 @@
+$(function(){
+   var $form = $('form'),
+        taskInput = $form.find('input[type=text]'),
+        taskDate = $form.find('input[type=date]'),
+        showTasksButton = $form.find('.showTasks'),
+        tasks = [],
+        templateContainer = $('#templates'),
+        taskTemplate;
+    
+    //busca en #templates  
+    templateContainer.find('#todoTemplate').load('/todo/task-template.html', function(){
+        taskTemplate = templateContainer.find('#todoTemplate').val();
+    });
+
+   $form.on('submit', onSubmit);
+   showTasksButton.on('click', showTasks);
+      
+   function onSubmit(){
+       
+       if(taskInput.val().length && taskDate.val().length){
+          tasks.push({
+             name:  taskInput.val(),
+             date: taskDate.val()
+          });
+       }
+       return false;
+   }
+   
+   function showTasks(){
+       var tasksHtml = '';//crea variable string taskHtml
+       for(var i=0; i< tasks.length; i++){//recorre el vector
+
+           tasksHtml += getTaskHtml(tasks[i]);//concateno todo lo de la funcion
+            console.log (tasksHtml);
+
+       }
+       
+       $form.find('ul').append(tasksHtml);
+
+   }
+   function getTaskHtml(task){
+      return taskTemplate
+        .replace(/%name%/g, task.name)
+        .replace(/%date%/g, task.date);
+  }
+});
